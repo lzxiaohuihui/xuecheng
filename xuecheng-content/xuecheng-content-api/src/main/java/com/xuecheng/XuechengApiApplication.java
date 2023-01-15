efbe0664cc5a64b035eb2035271661884c297828
lzxiaohuihui <895827938@qq.com>
2023-01-15 12:07:59 +0800

    create content project

---
+++ xuecheng-content/xuecheng-content-api/src/main/java/com/xuecheng/XuechengApiApplication.java
@@ -0,0 +1,13 @@
+package com.xuecheng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XuechengApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XuechengApiApplication.class, args);
+    }
+
+}
