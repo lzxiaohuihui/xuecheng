c7881ed2148301f79abf08c3e13a971075312a25
lzxiaohuihui <895827938@qq.com>
2023-02-04 21:20:47 +0800

    media module

---
+++ xuecheng-gateway/src/main/java/com/xuecheng/XuechengGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.xuecheng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XuechengGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XuechengGatewayApplication.class, args);
+    }
+
+}
