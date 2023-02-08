7498ff81d9abfc181f8b8e3654ff1d34d783ae90
lzxiaohuihui <895827938@qq.com>
2023-02-08 10:33:59 +0800

    upload media images

--- xuecheng-media/xuecheng-media-service/src/main/java/com/xuecheng/media/service/impl/MediaFileServiceImpl.java
+++ xuecheng-media/xuecheng-media-service/src/main/java/com/xuecheng/media/service/impl/MediaFileServiceImpl.java
@@ -1,48 +1,203 @@
 package com.xuecheng.media.service.impl;
 
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.api.R;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.google.errorprone.annotations.concurrent.UnlockMethod;
+import com.j256.simplemagic.ContentInfo;
+import com.j256.simplemagic.ContentInfoUtil;
+import com.xuecheng.base.exception.XueChengException;
 import com.xuecheng.base.model.PageParams;
 import com.xuecheng.base.model.PageResult;
 import com.xuecheng.media.mapper.MediaFilesMapper;
 import com.xuecheng.media.model.dto.QueryMediaParamsDto;
+import com.xuecheng.media.model.dto.UploadFileParamsDto;
+import com.xuecheng.media.model.dto.UploadFileResultDto;
 import com.xuecheng.media.model.po.MediaFiles;
 import com.xuecheng.media.service.MediaFileService;
+import io.minio.MinioClient;
+import io.minio.PutObjectArgs;
+import io.minio.errors.*;
+import org.apache.commons.codec.digest.DigestUtils;
 import org.apache.commons.lang3.StringUtils;
+import org.jetbrains.annotations.Nullable;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.util.Date;
 import java.util.List;
 
 /**
- * @description TODO
  * @author Mr.M
- * @date 2022/9/10 8:58
  * @version 1.0
+ * @description TODO
+ * @date 2022/9/10 8:58
  */
- @Service
+@Service
 public class MediaFileServiceImpl implements MediaFileService {
 
-  @Autowired
- MediaFilesMapper mediaFilesMapper;
+    @Autowired
+    MediaFilesMapper mediaFilesMapper;
 
- @Override
- public PageResult<MediaFiles> queryMediaFiels(Long companyId,PageParams pageParams, QueryMediaParamsDto queryMediaParamsDto) {
+    @Autowired
+    MinioClient minioClient;
 
-  //构建查询条件对象
-  LambdaQueryWrapper<MediaFiles> queryWrapper = new LambdaQueryWrapper<>();
-  
-  //分页对象
-  Page<MediaFiles> page = new Page<>(pageParams.getPageNo(), pageParams.getPageSize());
-  // 查询数据内容获得结果
-  Page<MediaFiles> pageResult = mediaFilesMapper.selectPage(page, queryWrapper);
-  // 获取数据列表
-  List<MediaFiles> list = pageResult.getRecords();
-  // 获取数据总数
-  long total = pageResult.getTotal();
-  // 构建结果集
-  PageResult<MediaFiles> mediaListResult = new PageResult<>(list, total, pageParams.getPageNo(), pageParams.getPageSize());
-  return mediaListResult;
+    @Value("${minio.bucket.files}")
+    private String bucket_files;
 
- }
+    @Autowired
+    private MediaFileService currentProxy;
+
+    @Override
+    public PageResult<MediaFiles> queryMediaFiels(Long companyId, PageParams pageParams, QueryMediaParamsDto queryMediaParamsDto) {
+
+        //构建查询条件对象
+        LambdaQueryWrapper<MediaFiles> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.like(MediaFiles::getFilename, queryMediaParamsDto.getFilename());
+        //分页对象
+        Page<MediaFiles> page = new Page<>(pageParams.getPageNo(), pageParams.getPageSize());
+        // 查询数据内容获得结果
+        Page<MediaFiles> pageResult = mediaFilesMapper.selectPage(page, queryWrapper);
+        // 获取数据列表
+        List<MediaFiles> list = pageResult.getRecords();
+        // 获取数据总数
+        long total = pageResult.getTotal();
+        // 构建结果集
+        PageResult<MediaFiles> mediaListResult = new PageResult<>(list, total, pageParams.getPageNo(), pageParams.getPageSize());
+        return mediaListResult;
+
+    }
+
+
+    @Override
+    public UploadFileResultDto uploadFile(Long companyId, UploadFileParamsDto dto, byte[] bytes, String folder, String objectName) {
+        String fileId = DigestUtils.md5Hex(bytes);
+
+        String filename = dto.getFilename();
+
+        if (StringUtils.isEmpty(objectName)) {
+            objectName = fileId + filename.substring(filename.lastIndexOf("."));
+        }
+
+        if (StringUtils.isEmpty(folder)) {
+            folder = getFileFolder(new Date(), true, true, true);
+        } else if (!folder.contains("/")) {
+            folder = folder + "/";
+        }
+
+        objectName = folder + objectName;
+        MediaFiles mediaFiles = null;
+
+        try {
+            addMediaFilesToMinIO(bytes, bucket_files, objectName);
+
+            mediaFiles = currentProxy.addMediaFilesToDb(companyId, fileId, dto,bucket_files, objectName);
+
+            UploadFileResultDto uploadFileResultDto = new UploadFileResultDto();
+            BeanUtils.copyProperties(mediaFiles, uploadFileResultDto);
+            return uploadFileResultDto;
+        } catch (Exception e) {
+            e.printStackTrace();
+            XueChengException.cast("上传过程中出错");
+        }
+
+        return null;
+    }
+
+    @Transactional
+    public MediaFiles addMediaFilesToDb(Long companyId,
+                                         String fileMd5,
+                                         UploadFileParamsDto dto,
+                                         String bucket,
+                                         String objectName
+    ) {
+        MediaFiles mediaFiles = mediaFilesMapper.selectById(fileMd5);
+
+        if (mediaFiles == null) {
+            mediaFiles = new MediaFiles();
+            BeanUtils.copyProperties(dto, mediaFiles);
+            BeanUtils.copyProperties(dto, mediaFiles);
+            mediaFiles.setId(fileMd5);
+            mediaFiles.setFileId(fileMd5);
+            mediaFiles.setCompanyId(companyId);
+            mediaFiles.setBucket(bucket);
+            mediaFiles.setFilePath(objectName);
+            mediaFiles.setUrl("/" + bucket + "/" + objectName);
+            mediaFiles.setCreateDate(LocalDateTime.now());
+            mediaFiles.setStatus("1");
+            mediaFiles.setAuditStatus("002003");
+
+            int insert = mediaFilesMapper.insert(mediaFiles);
+            if (insert < 0) {
+                XueChengException.cast("保存文件信息失败");
+            }
+        }
+
+        return mediaFiles;
+    }
+
+    private void addMediaFilesToMinIO(byte[] bytes, String bucket, String objectName) {
+        //资源的媒体类型
+        String contentType = MediaType.APPLICATION_OCTET_STREAM_VALUE;//默认未知二进制流
+
+        if (objectName.indexOf(".") >= 0) {
+            //取objectName中的扩展名
+            String extension = objectName.substring(objectName.lastIndexOf("."));
+            ContentInfo extensionMatch = ContentInfoUtil.findExtensionMatch(extension);
+            if (extensionMatch != null) {
+                contentType = extensionMatch.getMimeType();
+            }
+
+        }
+
+        ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(bytes);
+        try {
+            PutObjectArgs putObjectArgs = PutObjectArgs.builder().bucket(bucket).object(objectName)
+                    .stream(byteArrayInputStream, byteArrayInputStream.available(), -1)
+                    .contentType(contentType)
+                    .build();
+
+            minioClient.putObject(putObjectArgs);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            XueChengException.cast("上传文件到文件系统出错");
+        }
+    }
+
+    //根据日期拼接目录
+    private String getFileFolder(Date date, boolean year, boolean month, boolean day) {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        //获取当前日期字符串
+        String dateString = sdf.format(new Date());
+        //取出年、月、日
+        String[] dateStringArray = dateString.split("-");
+        StringBuffer folderString = new StringBuffer();
+        if (year) {
+            folderString.append(dateStringArray[0]);
+            folderString.append("/");
+        }
+        if (month) {
+            folderString.append(dateStringArray[1]);
+            folderString.append("/");
+        }
+        if (day) {
+            folderString.append(dateStringArray[2]);
+            folderString.append("/");
+        }
+        return folderString.toString();
+    }
+
+
 }
