b7a458189f38a466242d2d7bef81ac875352d44c
lzxiaohuihui <895827938@qq.com>
2023-02-16 19:27:02 +0800

    远程调用上传minio测试失败

--- xuecheng-content/xuecheng-content-api/src/main/java/com/xuecheng/content/XuechengApiApplication.java
+++ xuecheng-content/xuecheng-content-api/src/main/java/com/xuecheng/content/XuechengApiApplication.java
@@ -5,11 +5,13 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
 @EnableSwagger2Doc
 @SpringBootApplication
+@EnableFeignClients(basePackages = "com.xuecheng.content.feignclient")
 @MapperScan("com.xuecheng.content.mapper")
 @ComponentScan("com.xuecheng")
 public class XuechengApiApplication {
--- xuecheng-content/xuecheng-content-service/src/test/java/com/xuecheng/ContentApplication.java
+++ xuecheng-content/xuecheng-content-service/src/test/java/com/xuecheng/ContentApplication.java
@@ -3,8 +3,11 @@ package com.xuecheng;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.openfeign.FeignClient;
 
 @MapperScan("com.xuecheng.content.mapper")
+@EnableFeignClients(basePackages = {"com.xuecheng.content.feignclient"})
 @SpringBootApplication
 public class ContentApplication {
 
