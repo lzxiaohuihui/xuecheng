b622006cb6f18bf38ba12374e8a3e32dc42ab0c5
lzxiaohuihui <895827938@qq.com>
2023-02-19 11:23:41 +0800

    Jwt授权访问，获取Jwt用户信息

---
+++ xuecheng-content/xuecheng-content-api/src/main/java/com/xuecheng/content/config/ResourceServerConfig.java
@@ -0,0 +1,36 @@
+package com.xuecheng.content.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+
+import javax.annotation.Resource;
+
+@Configuration
+@EnableResourceServer
+public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
+    //资源服务标识
+    public static final String RESOURCE_ID = "xuecheng";
+    @Resource
+    TokenStore tokenStore;
+
+    @Override
+    public void configure(ResourceServerSecurityConfigurer resources) {
+        resources.resourceId(RESOURCE_ID)//资源 id
+                .tokenStore(tokenStore)
+                .stateless(true);
+    }
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/r/**", "/course/**").authenticated()//所有/r/**的请求必须认证通过
+                .anyRequest().permitAll()
+        ;
+    }
+
+}
---
+++ xuecheng-content/xuecheng-content-api/src/main/java/com/xuecheng/content/config/TokenConfig.java
@@ -0,0 +1,27 @@
+package com.xuecheng.content.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+@Configuration
+public class TokenConfig {
+    //jwt 签名密钥,与认证服务保持一致
+    private final String SIGNING_KEY = "mq123";
+
+    @Bean
+    public TokenStore tokenStore() {
+        //JWT 令牌存储方案
+        return new JwtTokenStore(accessTokenConverter());
+    }
+
+    @Bean
+    public JwtAccessTokenConverter accessTokenConverter() {
+        JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
+        converter.setSigningKey(SIGNING_KEY);
+        return converter;
+    }
+
+}
